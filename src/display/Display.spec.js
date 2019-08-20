9292eddb67d620fe65ceb77248dec6cbb0d7f975
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-20 17:30:54 -0400

    mvp

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -4,13 +4,23 @@ import { render } from '@testing-library/react';
 import Display from './Display';
 
 describe('<Display />', () => {
+  it('displays have green-led classes when unlocked and open', () => {
+    const { getByText } = render(<Display closed={false} locked={false} />);
+
+    const closed = getByText(/open/i);
+    const locked = getByText(/unlocked/i);
+    
+    expect(closed.className).toMatch(/green-led/);
+    expect(locked.className).toMatch(/green-led/);
+  });
+
   it('displays have red-led classes when locked and closed', () => {
     const { getByText } = render(<Display closed={true} locked={true} />);
 
     const closed = getByText(/closed/i);
     const locked = getByText(/locked/i);
     
-    expect(closed.classList).toContain('red-led');
-    expect(locked.classList).toContain('red-led');
+    expect(closed.className).toMatch(/red-led/);
+    expect(locked.className).toMatch(/red-led/);
   });
 });
