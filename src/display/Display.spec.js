6a31d70196f51b0e8d0aa0dbc02d1c2f86cc786d
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-08-20 11:55:07 -0400

    controls tested

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
-// Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+describe('<Display />', () => {
+  it('unlocked and open by default', () => {
+    const { getByText } = render(<Display />);
+
+    getByText(/^unlocked$/i);
+    getByText(/^open$/i);
+  });
+});
